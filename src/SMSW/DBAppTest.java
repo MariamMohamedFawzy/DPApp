8cab56fd8ee4db2acb1a9874d24b9020ce01469d
MariamMohamedFawzy <maryammohamed61@gmail.com>
2015-03-05 11:53:01 +0200

    updates

--- src/SMSW/DBAppTest.java
+++ src/SMSW/DBAppTest.java
@@ -6,53 +6,7 @@ import java.util.Iterator;
 
 public class DBAppTest {
 
-	public void init() {
+	
 
-	}
-
-	public Iterator selectFromTable(String strTable,
-			Hashtable<String, String> htblColNameValue, String strOperator)
-			throws DBEngineException {
-		if (strTable != null && !(strTable.equals(""))) {
-			if (htblColNameValue.size() > 0) {
-				if (strOperator.equals("AND")) {
-					
-				}
-				else if (strOperator.equals("OR")) {
-					
-				}
-				else {
-					
-				}
-			}
-			else {
-				return null;
-			}
-		}
-		else {
-			return null;
-		}
-		Enumeration<String> enumKey = htblColNameValue.keys();
-		while(enumKey.hasMoreElements()) {
-		    String key = enumKey.nextElement();
-		    String val = htblColNameValue.get(key);
-//		    if(key==0 && val.equals("0"))
-//		        table.remove(key);
-		}
-		return null;
-
-	}
-
-	public Iterator selectFromOneTable(String strTable, String key, String value) {
-		return null;
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
